aac907160dd1e2bbe9c3ae9d74a75981a9249255
Don Coleman <dcoleman@chariotsolutions.com>
2017-03-09 18:19:25 -0500

    one

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,78 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var MIDI_SERVICE_UUID = '03B80E5A-EDE8-4B33-A751-6CE34EC4C700';
+var MIDI_CHARACTERISTIC_UUID = '7772E5DB-3868-4112-A1A9-F2669D106BF3';
+
 var app = {
-    // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        this.bindEvents();
     },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        blePeripheral.onWriteRequest(app.didReceiveWriteRequest);
+        blePeripheral.onBluetoothStateChange(app.onBluetoothStateChange);
+        document.querySelector('#topLeft').addEventListener('touchstart', app.drum, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+       app.createServiceJSON();
 
-        console.log('Received Event: ' + id);
-    }
+   },
+   createServiceJSON: function() {
+
+       var property = blePeripheral.properties;
+       var permission = blePeripheral.permissions;
+
+       var midiService = {
+           uuid: MIDI_SERVICE_UUID,
+           characteristics: [
+               {
+                   uuid: MIDI_CHARACTERISTIC_UUID,
+                   properties: property.WRITE | property.READ | property.NOTIFY,
+                   permissions: permission.WRITEABLE | permission.READABLE,
+                   descriptors: [
+                       {
+                           uuid: '2901',
+                           value: 'MIDI'
+                       }
+                   ]
+               }
+           ]
+       };
+
+       Promise.all([
+           blePeripheral.createServiceFromJSON(midiService),
+           blePeripheral.startAdvertising(midiService.uuid, 'Cordova')
+       ]).then(
+           function() { console.log ('Created MIDI Service'); },
+           app.onError
+       );
+   },
+   drum: function() {
+        var drumOn = new Uint8Array([0x80, 0x80, 0x90 + 10, 41, 127]).buffer;
+        var drumOff = new Uint8Array([0x80, 0x80, 0x80 + 10, 41, 0]).buffer;
+        app.sendData(drumOn);
+        setTimeout(app.sendData, 15, drumOff);
+   },
+   sendData: function(buffer) {
+
+        var success = function() {
+            console.log('Sent ' + input.value);
+        };
+        var failure = function() {
+            console.log('Error sending.');
+        };
+
+        blePeripheral.setCharacteristicValue(MIDI_SERVICE_UUID, MIDI_CHARACTERISTIC_UUID, buffer).
+            then(success, failure);
+
+   },
+   didReceiveWriteRequest: function(request) {
+       console.log(request);
+   },
+   onBluetoothStateChange: function(state) {
+       console.log('Bluetooth State is', state);
+       outputDiv.innerHTML += 'Bluetooth  is ' +  state + '<br/>';
+   }
 };
 
 app.initialize();
