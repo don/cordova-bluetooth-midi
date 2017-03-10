889ab459f39871375cc38dbdcca4be1aa6531543
Don Coleman <dcoleman@chariotsolutions.com>
2017-03-09 19:00:01 -0500

    4 drums

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,5 @@
+// Cordova MIDI Drum Machine
+// (c) 2017 Don Coleman
 var MIDI_SERVICE_UUID = '03B80E5A-EDE8-4B33-A751-6CE34EC4C700';
 var MIDI_CHARACTERISTIC_UUID = '7772E5DB-3868-4112-A1A9-F2669D106BF3';
 
@@ -12,10 +14,12 @@ var app = {
 
         blePeripheral.onWriteRequest(app.didReceiveWriteRequest);
         blePeripheral.onBluetoothStateChange(app.onBluetoothStateChange);
-        document.querySelector('#topLeft').addEventListener('touchstart', app.drum, false);
-
         app.createServiceJSON();
 
+        document.querySelector('#topLeft').addEventListener('touchstart', app.floorTom, false);
+        document.querySelector('#topRight').addEventListener('touchstart', app.crashCymbal, false);
+        document.querySelector('#bottomLeft').addEventListener('touchstart', app.snare, false);
+        document.querySelector('#bottomRight').addEventListener('touchstart', app.base, false);
     },
     createServiceJSON: function () {
 
@@ -47,9 +51,28 @@ var app = {
             app.onError
             );
     },
-    drum: function () {
-        var drumOn = new Uint8Array([0x80, 0x80, 0x90 + 10, 41, 127]).buffer;
-        var drumOff = new Uint8Array([0x80, 0x80, 0x80 + 10, 41, 0]).buffer;
+    cowbell: function() {
+        app.drum(56, 127);
+    },
+    snare: function() {
+        app.drum(38, 127);
+    },
+    base: function() {
+        app.drum(36, 127);
+    },
+    floorTom: function() {
+        app.drum(41, 127);
+    },
+    rideCymbal: function() {
+        app.drum(51, 127);
+    },
+    crashCymbal: function() {
+        app.drum(49, 127);
+    },
+    drum: function (midiNote, velocity) {
+        var channel = 10;
+        var drumOn = new Uint8Array([0x80, 0x80, 0x90 + channel, midiNote, velocity]).buffer;
+        var drumOff = new Uint8Array([0x80, 0x80, 0x80 + channel, midiNote, 0]).buffer;
         app.sendData(drumOn);
         setTimeout(app.sendData, 15, drumOff);
     },
@@ -71,7 +94,6 @@ var app = {
     },
     onBluetoothStateChange: function (state) {
         console.log('Bluetooth State is', state);
-        outputDiv.innerHTML += 'Bluetooth  is ' + state + '<br/>';
     }
 };
 
